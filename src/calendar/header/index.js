7ea01512408a400717c91a10b46a05d4bb10a6b2
Mihovil <airmiha@gmail.com>
2018-08-10 23:06:32 +0200

    Add ability to hide header

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -5,10 +5,6 @@ import XDate from 'xdate';
 import PropTypes from 'prop-types';
 import styleConstructor from './style';
 import { weekDayNames } from '../../dateutils';
-import {
-  CHANGE_MONTH_LEFT_ARROW,
-  CHANGE_MONTH_RIGHT_ARROW
-} from '../../testIDs';
 
 class CalendarHeader extends Component {
   static propTypes = {
@@ -20,13 +16,7 @@ class CalendarHeader extends Component {
     firstDay: PropTypes.number,
     renderArrow: PropTypes.func,
     hideDayNames: PropTypes.bool,
-    weekNumbers: PropTypes.bool,
-    onPressArrowLeft: PropTypes.func,
-    onPressArrowRight: PropTypes.func
-  };
-
-  static defaultProps = {
-    monthFormat: 'MMMM yyyy',
+    weekNumbers: PropTypes.bool
   };
 
   constructor(props) {
@@ -34,8 +24,6 @@ class CalendarHeader extends Component {
     this.style = styleConstructor(props.theme);
     this.addMonth = this.addMonth.bind(this);
     this.substractMonth = this.substractMonth.bind(this);
-    this.onPressLeft = this.onPressLeft.bind(this);
-    this.onPressRight = this.onPressRight.bind(this);
   }
 
   addMonth() {
@@ -46,38 +34,6 @@ class CalendarHeader extends Component {
     this.props.addMonth(-1);
   }
 
-  shouldComponentUpdate(nextProps) {
-    if (
-      nextProps.month.toString('yyyy MM') !==
-      this.props.month.toString('yyyy MM')
-    ) {
-      return true;
-    }
-    if (nextProps.showIndicator !== this.props.showIndicator) {
-      return true;
-    }
-    if (nextProps.hideDayNames !== this.props.hideDayNames) {
-      return true;
-    }
-    return false;
-  }
-
-  onPressLeft() {
-    const {onPressArrowLeft} = this.props;
-    if(typeof onPressArrowLeft === 'function') {
-      return onPressArrowLeft(this.substractMonth);
-    }
-    return this.substractMonth();
-  }
-
-  onPressRight() {
-    const {onPressArrowRight} = this.props;
-    if(typeof onPressArrowRight === 'function') {
-      return onPressArrowRight(this.addMonth);
-    }
-    return this.addMonth();
-  }
-
   render() {
     let leftArrow = <View />;
     let rightArrow = <View />;
@@ -85,32 +41,29 @@ class CalendarHeader extends Component {
     if (!this.props.hideArrows) {
       leftArrow = (
         <TouchableOpacity
-          onPress={this.onPressLeft}
+          onPress={this.substractMonth}
           style={this.style.arrow}
-          hitSlop={{left: 20, right: 20, top: 20, bottom: 20}}
-          testID={CHANGE_MONTH_LEFT_ARROW}
         >
-          {this.props.renderArrow
-            ? this.props.renderArrow('left')
-            : <Image
-                source={require('../img/previous.png')}
-                style={this.style.arrowImage}
-              />}
+          {this.props.renderArrow ? (
+            this.props.renderArrow('left')
+          ) : (
+            <Image
+              source={require('../img/previous.png')}
+              style={this.style.arrowImage}
+            />
+          )}
         </TouchableOpacity>
       );
       rightArrow = (
-        <TouchableOpacity
-          onPress={this.onPressRight}
-          style={this.style.arrow}
-          hitSlop={{left: 20, right: 20, top: 20, bottom: 20}}
-          testID={CHANGE_MONTH_RIGHT_ARROW}
-        >
-          {this.props.renderArrow
-            ? this.props.renderArrow('right')
-            : <Image
-                source={require('../img/next.png')}
-                style={this.style.arrowImage}
-              />}
+        <TouchableOpacity onPress={this.addMonth} style={this.style.arrow}>
+          {this.props.renderArrow ? (
+            this.props.renderArrow('right')
+          ) : (
+            <Image
+              source={require('../img/next.png')}
+              style={this.style.arrowImage}
+            />
+          )}
         </TouchableOpacity>
       );
     }
@@ -120,25 +73,30 @@ class CalendarHeader extends Component {
     }
     return (
       <View>
-        <View style={this.style.header}>
-          {leftArrow}
-          <View style={{ flexDirection: 'row' }}>
-            <Text allowFontScaling={false} style={this.style.monthText} accessibilityTraits='header'>
-              {this.props.month.toString(this.props.monthFormat)}
-            </Text>
-            {indicator}
+        {!this.props.hideMonth && (
+          <View style={this.style.header}>
+            {leftArrow}
+            <View style={{ flexDirection: 'row' }}>
+              <Text style={this.style.monthText}>
+                {this.props.month.toString(
+                  this.props.monthFormat ? this.props.monthFormat : 'MMMM yyyy'
+                )}
+              </Text>
+              {indicator}
+            </View>
+            {rightArrow}
           </View>
-          {rightArrow}
-        </View>
-        {
-          !this.props.hideDayNames &&
+        )}
+        {!this.props.hideDayNames && (
           <View style={this.style.week}>
-            {this.props.weekNumbers && <Text allowFontScaling={false} style={this.style.dayHeader}></Text>}
+            {this.props.weekNumbers && <Text style={this.style.dayHeader} />}
             {weekDaysNames.map((day, idx) => (
-              <Text allowFontScaling={false} key={idx} accessible={false} style={this.style.dayHeader} numberOfLines={1} importantForAccessibility='no'>{day}</Text>
+              <Text key={idx} style={this.style.dayHeader} numberOfLines={1}>
+                {day}
+              </Text>
             ))}
           </View>
-        }
+        )}
       </View>
     );
   }
